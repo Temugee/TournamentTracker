76608145867611c9e75354ba23e5b02fff46f6d5
Temuge <temuge.erdene@gmail.com>
2024-04-05 12:46:01 +0800

    a bit imp?

--- TrackerUI/TournamentViewerForm.Designer.cs
+++ TrackerUI/TournamentViewerForm.Designer.cs
@@ -29,22 +29,91 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TournamentViewerForm));
+            headerLabel = new Label();
+            touramentName = new Label();
+            roundLabel = new Label();
+            roundDropDown = new ComboBox();
+            unplayedOnlyCheckBox = new CheckBox();
             SuspendLayout();
+            // 
+            // headerLabel
+            // 
+            headerLabel.AutoSize = true;
+            headerLabel.Location = new Point(14, 9);
+            headerLabel.Margin = new Padding(5, 0, 5, 0);
+            headerLabel.Name = "headerLabel";
+            headerLabel.Size = new Size(214, 50);
+            headerLabel.TabIndex = 0;
+            headerLabel.Text = "Tournament:";
+            // 
+            // touramentName
+            // 
+            touramentName.AutoSize = true;
+            touramentName.Location = new Point(229, 9);
+            touramentName.Margin = new Padding(5, 0, 5, 0);
+            touramentName.Name = "touramentName";
+            touramentName.Size = new Size(150, 50);
+            touramentName.TabIndex = 1;
+            touramentName.Text = "<none>";
+            // 
+            // roundLabel
+            // 
+            roundLabel.AutoSize = true;
+            roundLabel.Font = new Font("Segoe UI Light", 15.75F);
+            roundLabel.Location = new Point(14, 88);
+            roundLabel.Name = "roundLabel";
+            roundLabel.Size = new Size(71, 30);
+            roundLabel.TabIndex = 2;
+            roundLabel.Text = "Round";
+            // 
+            // roundDropDown
+            // 
+            roundDropDown.Font = new Font("Segoe UI Light", 15.75F);
+            roundDropDown.FormattingEnabled = true;
+            roundDropDown.Location = new Point(116, 85);
+            roundDropDown.Name = "roundDropDown";
+            roundDropDown.Size = new Size(188, 38);
+            roundDropDown.TabIndex = 3;
+            // 
+            // unplayedOnlyCheckBox
+            // 
+            unplayedOnlyCheckBox.AutoSize = true;
+            unplayedOnlyCheckBox.FlatStyle = FlatStyle.Flat;
+            unplayedOnlyCheckBox.Font = new Font("Segoe UI Light", 15.75F);
+            unplayedOnlyCheckBox.Location = new Point(138, 164);
+            unplayedOnlyCheckBox.Name = "unplayedOnlyCheckBox";
+            unplayedOnlyCheckBox.Size = new Size(160, 34);
+            unplayedOnlyCheckBox.TabIndex = 4;
+            unplayedOnlyCheckBox.Text = "Unplayed Only";
+            unplayedOnlyCheckBox.UseVisualStyleBackColor = true;
             // 
             // TournamentViewerForm
             // 
-            AutoScaleDimensions = new SizeF(12F, 30F);
+            AutoScaleDimensions = new SizeF(19F, 50F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.White;
-            ClientSize = new Size(566, 317);
-            Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            ClientSize = new Size(887, 540);
+            Controls.Add(unplayedOnlyCheckBox);
+            Controls.Add(roundDropDown);
+            Controls.Add(roundLabel);
+            Controls.Add(touramentName);
+            Controls.Add(headerLabel);
+            Font = new Font("Segoe UI Light", 27.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            ForeColor = Color.FromArgb(51, 153, 255);
             Icon = (Icon)resources.GetObject("$this.Icon");
-            Margin = new Padding(5, 6, 5, 6);
+            Margin = new Padding(8, 10, 8, 10);
             Name = "TournamentViewerForm";
             Text = "Tournament Viewer";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label headerLabel;
+        private Label touramentName;
+        private Label roundLabel;
+        private ComboBox roundDropDown;
+        private CheckBox unplayedOnlyCheckBox;
     }
 }
